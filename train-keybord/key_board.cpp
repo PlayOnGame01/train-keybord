12f9d152c4044f2c61e3448a26d755bd174ac900
Artem <artemzhdanov1299@gmail.com>
2022-12-22 21:59:26 +0200

    Add project files.

---
+++ train-keybord/key_board.cpp
@@ -0,0 +1,214 @@
+#include<windows.h>
+#include <windowsX.h>
+#include <commctrl.h>
+#include <tchar.h>
+#include <conio.h>
+#include <time.h>
+#include"resource.h"
+#pragma comment(lib,"comctl32")
+#define WM_ICON WM_APP
+#define ID_TRAYICON WM_USER
+#define WM_USER_SHELLICON (WM_USER + 1)
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+PNOTIFYICONDATA pNID;
+NOTIFYICONDATA data;
+
+HWND hTime, hLevel, hDialog, hText, hEnterText, hFinish;
+HWND hEdit1;
+HWND hButton16;
+HWND hModalDialog, hResultDialog;
+
+HICON hIcon;
+
+HANDLE hMutex;
+HANDLE hThread;
+
+TCHAR str_time[60];
+TCHAR str_result[100];
+TCHAR str_text[50];
+TCHAR str_timer[60];
+
+int points = 0;
+int error = 0;
+
+TCHAR lvl1[] = TEXT("asas ssaa assa aass sasa saas");
+TCHAR lvl2[] = TEXT("dfdf ddff ffdd fddf fdfd dffd");
+TCHAR lvl3[] = TEXT("прпр рпрп ппрр ррпп рппр пррп");
+TCHAR str_lvl[10];
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow) {
+	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+void OnTrayIcon(WPARAM wp, LPARAM lp) {
+	if (lp == WM_LBUTTONDBLCLK) {
+		Shell_NotifyIcon(NIM_DELETE, pNID); // Удаляем иконку из трэя
+		ShowWindow(hDialog, SW_NORMAL); // Восстанавливаем окно
+		SetForegroundWindow(hDialog); // устанавливаем окно на передний план
+	}
+}
+
+void Cls_OnSize(HWND hwnd, UINT state, int cx, int cy) {
+	if (state == SIZE_MINIMIZED) {
+		ShowWindow(hwnd, SW_HIDE); // Прячем окно
+		Shell_NotifyIcon(NIM_ADD, pNID); // Добавляем иконку в трэй
+	}
+}
+
+DWORD WINAPI TimeFunc(LPVOID lp) {
+	HWND hProgress = (HWND)lp;
+	struct Time {
+		int Minutes = 0;
+		int Seconds = 0;
+	} Time;
+
+	while (TRUE) {
+		wsprintf(str_time, TEXT("%d:%d"), Time.Minutes, Time.Seconds);
+		SetWindowText(hTime, str_time);
+		if (Time.Seconds >= 60) // Ну тут 60 секунд одна минута ну это понятно
+		{
+			Time.Seconds = 00;
+			Time.Minutes++;
+		}
+		else {
+			Time.Seconds++;
+		}
+		Sleep(5000);//Ожидание 5 секунд
+	}
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG: {
+		hTime = GetDlgItem(hWnd, IDC_TIME);//окно времени
+		hEdit1 = GetDlgItem(hWnd, IDC_EDIT1);
+		hLevel = GetDlgItem(hWnd, IDC_LEVEL);//окно уровня
+		hText = GetDlgItem(hWnd, IDC_TEXT);//Строка текста
+		hEnterText = GetDlgItem(hWnd, IDC_ENTERTEXT);//текст для ввода 
+		hFinish = GetDlgItem(hWnd, IDOK);//кнопка старта 
+		hDialog = hWnd;
+		hButton16 = GetDlgItem(hWnd, IDC_BUTTON16);
+
+		// Получим дескриптор экземпляра приложения
+		HINSTANCE hInst = GetModuleHandle(NULL);
+		pNID->cbSize = sizeof(NOTIFYICONDATA); //размер структуры
+		memset(pNID, 0, sizeof(NOTIFYICONDATA)); //обнулим ка мы структуру
+		pNID->hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1)); //загружаем пользовательскую иконку
+		SetClassLong(hDialog, GCL_HICON, LONG(hIcon)); // устанавливаем иконку в главном окне приложения
+		pNID->hWnd = hDialog; //дескриптор окна, которое будет получать уведомляющие сообщения, ассоциированные с иконкой в трее.	
+		pNID->uCallbackMessage = WM_ICON;
+		pNID->uFlags = NIF_TIP | NIF_ICON | NIF_MESSAGE | NIF_INFO;
+		pNID->uID = ID_TRAYICON;
+
+		TCHAR GUID[] = TEXT("{D98CD3E0-670D-4def-9B74-99FE6E683BFD}");
+		hMutex = CreateMutex(NULL, FALSE, GUID);
+		DWORD dwAnswer = WaitForSingleObject(hMutex, 0);
+		if (dwAnswer == WAIT_TIMEOUT) {
+			MessageBox(hWnd, TEXT("Стоп не льзя запускать больше одного, а то по попе бо-бо!!!"), TEXT("Тренировка пальчиков"), MB_OK | MB_ICONINFORMATION);
+			EndDialog(hWnd, 0);
+		}
+	}
+    break;
+	case WM_COMMAND: {
+		if (LPARAM(wParam) == IDC_START) {
+			LRESULT result1 = SendDlgItemMessage(hWnd, IDC_RADIO1, BM_GETCHECK, 0, 0);//Результат 1 уровня 
+			LRESULT result2 = SendDlgItemMessage(hWnd, IDC_RADIO2, BM_GETCHECK, 0, 0);//Результат 2 уровня
+			LRESULT result3 = SendDlgItemMessage(hWnd, IDC_RADIO3, BM_GETCHECK, 0, 0);//Результат 3 уровня
+
+			if (result1 == BST_CHECKED) {
+				hModalDialog = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DLGPROC(DlgProc));
+				ShowWindow(hModalDialog, SW_RESTORE);
+				SetWindowText(hText, lvl1);
+				SendMessage(hLevel, WM_SETTEXT, 0, LPARAM(TEXT("1")));
+				wsprintf(str_lvl, TEXT("%s"), TEXT("1"));//Сам 1 уровень
+			}
+			if (result2 == BST_CHECKED) {
+				hModalDialog = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DLGPROC(DlgProc));
+				ShowWindow(hModalDialog, SW_RESTORE);
+				SetWindowText(hText, lvl2);
+				SendMessage(hLevel, WM_SETTEXT, 0, LPARAM(TEXT("2")));
+				wsprintf(str_lvl, TEXT("%s"), TEXT("2"));//Сам 2 уровень
+			}
+			if (result3 == BST_CHECKED) {
+				hModalDialog = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DLGPROC(DlgProc));
+				ShowWindow(hModalDialog, SW_RESTORE);
+				SetWindowText(hText, lvl3);
+				SendMessage(hLevel, WM_SETTEXT, 0, LPARAM(TEXT("3")));
+				wsprintf(str_lvl, TEXT("%s"), TEXT("3"));//Сам 3 уровень
+			}
+
+			hThread = CreateThread(NULL, 0, TimeFunc, hTime, 0, NULL);
+
+			TCHAR str_1lvl[10] = TEXT("1");
+			TCHAR str_2lvl[10] = TEXT("2");
+			TCHAR str_3lvl[10] = TEXT("3");
+
+			int compare = _tcscmp(str_lvl, str_1lvl);
+			if (compare == 0) {
+				wsprintf(str_text, TEXT("%s"), lvl1);
+			}
+			else {
+				compare = _tcscmp(str_lvl, str_2lvl);
+				if (compare == 0) {
+					wsprintf(str_text, TEXT("%s"), lvl2);
+				}
+				else {
+					compare = _tcscmp(str_lvl, str_3lvl);
+					if (compare == 0) {
+						wsprintf(str_text, TEXT("%s"), lvl3);
+					}
+				}
+			}
+
+		}
+
+		if (LPARAM(wParam) == IDOK) {
+			points = 0;
+			error = 0;
+
+			int count_result = GetWindowTextLength(hEnterText);
+			GetWindowText(hEnterText, str_result, count_result + 1);
+			for (int i = 0; i < 50; i++) {
+				if (str_result[i] == str_text[i]) {
+					points++;
+				}
+				else {
+					error++;
+				}//Конечный резулятат где максимум 50
+			}
+			TCHAR str_points[10];
+			wsprintf(str_points, TEXT("%d"), points);
+			TCHAR str_error[10];
+			wsprintf(str_error, TEXT("%d"), error);
+
+			CloseHandle(hThread);
+			EndDialog(hModalDialog, 0);
+			ShowWindow(hResultDialog, SW_RESTORE);
+			SendMessage(hEdit1, WM_SETTEXT, 0, LPARAM(str_time));
+		}
+	}
+				   break;
+	case WM_SIZE: {
+		if (message == SIZE_MINIMIZED) {
+			ShowWindow(hWnd, SW_HIDE); // Прячем окно
+			Shell_NotifyIcon(NIM_ADD, pNID); // Добавляем иконку в трэй
+		}
+	}
+				break;
+				//HANDLE_MSG(hWnd, WM_SIZE, Cls_OnSize);
+
+	case WM_CLOSE:
+		EndDialog(hWnd, 0);
+		return TRUE;
+	}
+	if (message == WM_ICON) {
+		OnTrayIcon(wParam, lParam);
+		return TRUE;
+	}
+	return FALSE;
+}
---
+++ train-keybord/resource.h
@@ -0,0 +1,70 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by train-keybord.rc
+//
+#define IDD_DIALOG1                     101
+#define IDD_DIALOG2                     103
+#define IDI_ICON1                       108
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON4                     1002
+#define IDC_BUTTON5                     1003
+#define IDC_BUTTON2                     1004
+#define IDC_BUTTON6                     1004
+#define IDC_BUTTON7                     1005
+#define IDC_BUTTON8                     1006
+#define IDC_BUTTON9                     1007
+#define IDC_BUTTON10                    1008
+#define IDC_BUTTON11                    1009
+#define IDC_BUTTON12                    1010
+#define IDC_BUTTON13                    1011
+#define IDC_BUTTON14                    1012
+#define IDC_BUTTON15                    1016
+#define IDC_BUTTON16                    1017
+#define IDC_BUTTON17                    1018
+#define IDC_BUTTON18                    1019
+#define IDC_BUTTON19                    1020
+#define IDC_BUTTON20                    1021
+#define IDC_BUTTON21                    1022
+#define IDC_BUTTON22                    1023
+#define IDC_BUTTON23                    1024
+#define IDC_BUTTON24                    1025
+#define IDC_BUTTON26                    1027
+#define IDC_BUTTON27                    1028
+#define IDC_BUTTON28                    1029
+#define IDC_BUTTON29                    1030
+#define IDC_BUTTON30                    1031
+#define IDC_BUTTON31                    1032
+#define IDC_BUTTON32                    1033
+#define IDC_BUTTON33                    1034
+#define IDC_BUTTON34                    1035
+#define IDC_BUTTON35                    1036
+#define IDC_BUTTON37                    1038
+#define IDC_BUTTON38                    1039
+#define IDC_BUTTON39                    1040
+#define IDC_BUTTON40                    1041
+#define IDC_BUTTON41                    1042
+#define IDC_BUTTON42                    1043
+#define IDC_BUTTON43                    1044
+#define IDC_BUTTON51                    1052
+#define IDC_EDIT1                       1056
+#define IDC_TEXT                        1057
+#define IDC_EDIT3                       1058
+#define IDC_TIME                        1058
+#define IDC_LEVEL                       1059
+#define IDC_BUTTON55                    1060
+#define IDC_RADIO1                      1061
+#define IDC_RADIO2                      1062
+#define IDC_ENTERTEXT                   1062
+#define IDC_RADIO3                      1063
+#define IDC_START                       1064
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1065
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
